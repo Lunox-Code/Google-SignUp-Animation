4e955f8c31013b129ee350f6dbb02c37ff5d8150
lunox-code <lunox@gmail.com>
2019-11-18 21:40:29 -0600

    Firebase Up!

--- app/src/main/java/com/example/googleloginandanimation/animation.java
+++ app/src/main/java/com/example/googleloginandanimation/animation.java
@@ -1,14 +1,43 @@
 package com.example.googleloginandanimation;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.V;
 
 public class animation extends AppCompatActivity {
+
+    Button sign_out;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_animation);
+
+        sign_out = findViewById(R.id.signout);
+
+        sign_out.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                //startActivity(new Intent(act_menu.this,MainActivity.class));
+                Intent i = new Intent(animation.this, MainActivity.class);
+                startActivity(i);
+                finish();
+                Toast.makeText(animation.this,"Cierre de sesion Exitoso!!",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
